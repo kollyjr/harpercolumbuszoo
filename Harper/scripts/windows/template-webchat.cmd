da47187fc7cafe54c87253a8494f7f04daed2851
unknown <kollyjr@icloud.com>
2020-11-01 19:33:42 -0500

    changes to get the bot online

--- Harper/scripts/windows/template-webchat.cmd
+++ Harper/scripts/windows/template-webchat.cmd
@@ -2,5 +2,5 @@
 
 SET PYTHONPATH=..\..\src;%PYTHONPATH%
 
-python -m templatey.clients.restful.flask.webchat.client --config ..\..\config\windows\config.webchat.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
+python -m programy.clients.restful.flask.webchat.client --config ..\..\config\windows\config.webchat.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
 
